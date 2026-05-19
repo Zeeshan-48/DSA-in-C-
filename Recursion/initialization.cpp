e0ef1200b1bd1cc9c176028e927b171f9d159e08
Zeeshan Anwer <zeshan1234anwar@gmail.com>
2026-05-19 15:07:59 +0530

    added recursion file

---
+++ Recursion/initialization.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void fun(int n){
+    if(n==0) return;
+    cout<<"good morning"<<endl;
+    fun(n-1);
+}
+int main(){
+    int n;
+    cout<<"enter n"<<endl;
+    cin>>n;
+    fun(n);
+   return 0; 
+}
