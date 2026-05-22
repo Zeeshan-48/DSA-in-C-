d72b8e940143baa23e86d0e458a49d1458a241f5
Zeeshan Anwer <zeshan1234anwar@gmail.com>
2026-05-22 14:09:19 +0530

    added new file to recursion

---
+++ Recursion/power.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+using namespace std;
+int power(int a, int b){
+    if(b==0) return 1;
+    return a*power(a,b-1);
+}
+int main(){
+    int a,b;
+    cout<<"enter the base ";
+    cin>>a;
+    cout<<"enter the exponent ";
+    cin>>b;
+    cout<<a<<" raised to power "<<b<<" is "<<power(a,b);
+    return 0;
+}
