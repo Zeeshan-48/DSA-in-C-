760323f2b4d4932bddff6dbe4eec867685133c65
Zeeshan Anwer <zeshan1234anwar@gmail.com>
2026-06-04 17:04:05 +0530

    added new file in recursion

---
+++ Recursion/palindrome.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+bool isPalindrome(string str, int i, int j){
+    if(i>j) return true;
+    if(str[i]!=str[j]) return false;
+    return isPalindrome(str,i+1,j-1);
+    
+}
+int main(){
+    string str = "racecar";
+    cout<<isPalindrome(str,0,str.length()-1);
+}
