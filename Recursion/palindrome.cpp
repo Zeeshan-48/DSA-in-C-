e735527edb3dac1f2e7cb8d35218c01d2c012281
Zeeshan Anwer <zeshan1234anwar@gmail.com>
2026-06-05 13:21:38 +0530

    edited palindrrome file

--- Recursion/palindrome.cpp
+++ Recursion/palindrome.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<string>
 using namespace std;
+// recursion method
 bool isPalindrome(string str, int i, int j){
     if(i>j) return true;
     if(str[i]!=str[j]) return false;
@@ -10,4 +11,17 @@ bool isPalindrome(string str, int i, int j){
 int main(){
     string str = "racecar";
     cout<<isPalindrome(str,0,str.length()-1);
+
+    // // iterative method
+    // int i=0; int j=str.length()-1;
+    // bool flag = true;
+    // while(i<=j){
+    //    if(str[i]!=str[j]){
+    //     flag = false;
+    //     break;
+    //    }
+    //     i++;
+    //     j--;
+    // }
+    // cout<<flag;
 }
