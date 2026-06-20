61902e4cbd567bf28eea8969d4609173cfd9597b
Zeeshan Anwer <zeshan1234anwar@gmail.com>
2026-06-20 16:55:52 +0530

    adde new file in Sorting

--- Sorting/zerosToEnd.cpp
+++ Sorting/zerosToEnd.cpp
@@ -1,18 +1,18 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int arr [] = {5,3,4,2,1};
-    int n = 5;
+    int arr [] = {5,4,0,3,0,6,0,1};
+    int n = 8;
     for(int i=0;i<n-1;i++){
         bool flag = true;
 
         for(int j=0;j<n-1-i;j++){
-            if(arr[j]>arr[j+1]){
-            swap(arr[j],arr[j+1]);
-            flag = false;
-            }
+            if(arr[j]==0){
+                swap(arr[j],arr[j+1]);
+                flag = false;
+            }  
         }
-        
+
         if(flag==true) break;
     }
     for(int i=0;i<n;i++){
