77983456632dded9a70a80fba7fa4f2eeaa2d695
Zeeshan Anwer <zeshan1234anwar@gmail.com>
2026-06-30 16:53:44 +0530

    added new files in Sorting

---
+++ Sorting/mergeSort.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void merge(vector<int>& a, vector<int>& b, vector<int>& v){
+    int i=0;
+    int j=0;
+    int k=0;
+    while(i<a.size() && j<b.size()){
+        if(a[i]<b[j]){
+            v[k] = a[i];
+            k++;
+            i++;
+        }
+        else{
+            v[k] = b[j];
+            k++;
+            j++;
+        }
+    }
+    if(i==a.size()){
+        while(j<b.size()){
+            v[k] = b[j];
+            k++;
+            j++;
+        }
+    }
+    if(j==b.size()){
+        while(i<a.size()){
+            v[k] = a[i];
+            k++;
+            i++;
+        }
+    }
+}
+void mergeSort(vector<int>& v){
+    int n = v.size();
+    if(n==1) return;
+    int n1 = n/2;
+    int n2 = n-n/2;
+    vector<int> a(n1);
+    vector<int> b(n2);
+    // filling a
+    for(int i=0;i<n1;i++){
+        a[i] = v[i];
+    }
+    // fillingh b
+    for(int i=0;i<n2;i++){
+        b[i] = v[i+n1];
+    }
+    mergeSort(a);
+    mergeSort(b);
+
+    merge(a,b,v);
+
+}
+int main(){
+    int arr[] = {3,1,5,2,6,8,7};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    vector<int> v(arr,arr+n);
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+
+    mergeSort(v);
+
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<" ";
+    }
+
+} 
