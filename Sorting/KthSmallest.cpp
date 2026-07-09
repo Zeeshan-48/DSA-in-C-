4f5ef5f152cfebce5f32c7077bfad494560831ec
Zeeshan Anwer <zeshan1234anwar@gmail.com>
2026-07-09 15:18:47 +0530

    added new file in sorting

--- Sorting/KthSmallest.cpp
+++ Sorting/KthSmallest.cpp
@@ -1,48 +1,37 @@
 #include<iostream>
 using namespace std;
 int partition(int arr[], int startIdx, int endIdx){
+    int pivotIdx = 0;
     int pivotElement = arr[startIdx];
     int count = 0;
     for(int i=startIdx+1;i<=endIdx;i++){
-        if(arr[i]<=pivotElement){
-            count++;
-        }
+        if(arr[i]<pivotElement) count++;
     }
-    int pivotIdx = startIdx + count;
+    pivotIdx = startIdx + count;
     swap(arr[startIdx],arr[pivotIdx]);
-
+     
     int i = startIdx;
     int j = endIdx;
     while(i<pivotIdx && j>pivotIdx){
         if(arr[i]<arr[pivotIdx]) i++;
-        if(arr[j]>arr[pivotIdx]) j--;
+        if(arr[j]>arr[pivotIdx]) j++;
         if(arr[i]>arr[pivotIdx] && arr[j]<=arr[pivotIdx]){
             swap(arr[i],arr[j]);
             i++;
             j--;
         }
     }
-
     return pivotIdx;
 }
-void quickSort(int arr[], int startIdx, int endIdx){
-    if(startIdx>=endIdx) return;
-    int pivotIdx = partition(arr, startIdx, endIdx);
-    quickSort(arr, startIdx, pivotIdx-1);
-    quickSort(arr, pivotIdx+1, endIdx);
+int kthSmallest(int arr[], int startIdx, int endIdx, int k){
+    int pivotIdx = partition(arr,startIdx,endIdx);
+    if(pivotIdx+1 == k) return arr[pivotIdx];
+    else if(pivotIdx+1 < k) return kthSmallest(arr,pivotIdx+1,endIdx,k);
+    else return kthSmallest(arr,startIdx,pivotIdx-1,k);
 }
 int main(){
-    // int arr[]={5,2,3,7,8,1,0};
     int arr[] = {3,6,5,4,8,9,1};
     int n = sizeof(arr)/sizeof(arr[0]);
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
-
-    quickSort(arr,0,n-1);
-
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }
+    int k = 4;
+    cout<<kthSmallest(arr,0,n-1,k);
 }
